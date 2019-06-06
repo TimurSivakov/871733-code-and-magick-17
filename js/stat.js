ae247e5ac4a880b40316fbc01f17e8d64a605ee0
Timur <tsivakov@yandex.ru>
2019-06-06 14:59:33 +0300

    заменил координаты объявленными переменными

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,14 @@
 
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 10;
+var BAR_X = 140;
+var BAR_Y = 80;
+var BAR_HEIGHT = 150;
+var BAR_WIDTH = 50;
+var BAR_GAP = 50;
 
 var renderCloud = function(ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -14,9 +22,19 @@ var renderText = function(ctx, text, x, y, font, color) {
 };
 
 window.renderStatistics = function(ctx) {
-  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, 100, 10, '#fff');
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
   renderText(ctx, 'Ура, вы победили', 130, 40, '16px PT Mono', '#000');
   renderText(ctx, 'Список результатов:', 130, 60);
+
+  ctx.fillStyle = '#000';
+  ctx.fillText('Вы', CLOUD_X + (GAP * 4) + (BAR_WIDTH + BAR_GAP) * 0, BAR_Y + BAR_HEIGHT + GAP * 2);
+  ctx.fillRect(BAR_X + (BAR_WIDTH + BAR_GAP) * 0, BAR_Y, BAR_WIDTH, BAR_HEIGHT);
+
+  ctx.fillText('Кекс', CLOUD_X + (GAP * 4) + (BAR_WIDTH + BAR_GAP) * 1, BAR_Y + BAR_HEIGHT + GAP * 2);
+  ctx.fillRect(BAR_X + (BAR_WIDTH + BAR_GAP) * 1, BAR_Y, BAR_WIDTH, BAR_HEIGHT);
+
+  ctx.fillText('Катя', CLOUD_X + (GAP * 4) + (BAR_WIDTH + BAR_GAP) * 2, BAR_Y + BAR_HEIGHT + GAP * 2);
+  ctx.fillRect(BAR_X + (BAR_WIDTH + BAR_GAP) * 2, BAR_Y, BAR_WIDTH, BAR_HEIGHT);
 };
